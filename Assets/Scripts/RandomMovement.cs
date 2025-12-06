c9615bb2ecfda6b291aff5683a497264dcbc1505
Jasper <Delfinado-J@ulster.ac.uk>
2025-12-06 23:49:12 +0000

    Added colliders to walls and objects

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -20,7 +20,7 @@ public class RandomMovement : MonoBehaviour
 
     void Update()
     {
-        if (agent.remainingDistance <= agent.stoppingDistance) //done with path
+        if (agent.remainingDistance <= agent.stoppingDistance) //done with path 
         {
             Vector3 point;
             if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
